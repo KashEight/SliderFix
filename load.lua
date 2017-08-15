1cfcc96c7d15db0ee0326b46d5d1eb6301f35dc3
kash <kashiwa.electory@gmail.com>
2017-08-15 20:21:08 +0900

    Update: load.lua Add "mod checker", which does not run when there are not other mods.

--- load.lua
+++ load.lua
@@ -2,10 +2,20 @@ if not Steam then
     return
 end
 
+for i, keys in ipairs(file.GetDirectories("mods/")) do
+    if keys:lower() ~= "base" or "downloads" or "logs" or "saves" or "sliderfix" then
+        break
+    elseif i > 5 then
+        break
+    else 
+        return
+    end
+end
+
 function doScript(script)
     local baseScript = script:lower()
     local hook_files = {
-        ["core/lib/managers/menu/items/coremenuitemslider"] = CoreMenuItemSlider.lua
+        ["core/lib/managers/menu/items/coremenuitemslider"] = "CoreMenuItemSlider.lua"
     }
     if hook_files[baseScript] then
         local fileName = ModPath .. "lua/" .. hook_files[baseScript]
