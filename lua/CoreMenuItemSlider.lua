1756d596c87d5a9bc649c451aeef9deb45f9f4b7
kash <kashiwa.electory@gmail.com>
2017-08-17 22:46:26 +0900

    Fix: Whitespace

--- lua/CoreMenuItemSlider.lua
+++ lua/CoreMenuItemSlider.lua
@@ -2,6 +2,6 @@ core:module("CoreMenuItemSlider")
 local set_value_original = ItemSlider.set_value
 
 function ItemSlider:set_value(value)
-	value = self._min + self._step * math.round((value - self._min) / self._step)
+    value = self._min + self._step * math.round((value - self._min) / self._step)
     set_value_original(self, value)
 end
